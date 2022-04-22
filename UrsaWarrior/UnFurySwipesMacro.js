00c324d7f8fcaaa1680d85dd3a11212e6a57666e
TeachingBudha <teachinggithub@elportalito.com>
2022-04-22 13:06:24 -0500

    First version of Macros for Ursa Warrior

--- UrsaWarrior/UnFurySwipesMacro.js
+++ UrsaWarrior/UnFurySwipesMacro.js
@@ -1,3 +1,9 @@
+//This one is to clear the mwak damage accumulated by Fury swipes
+//Unfortunately the way I make it work for the Ursa Warrior is to execute it before
+//Before his Axe/Bite/Claw attacks. In order to ensure that Ursa Warrior attacks which are
+//not Fury Swipes do the right damage
+
+
 //Either the player or the GM
 const ursaActor= game.user.character || canvas.tokens.controlled[0].actor;
 
